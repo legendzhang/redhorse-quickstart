7bc461f89eb32a6a70eef75f1aa478558889c0d3
legendzhang <openplatform@sohu.com>
2010-08-02 19:00:23 +0800

    调整程序选择方式为grid
    Signed-off-by: legendzhang <openplatform@sohu.com>

--- src/com/redhorse/quickstart/AppConfig.java
+++ src/com/redhorse/quickstart/AppConfig.java
@@ -19,11 +19,12 @@ package com.redhorse.quickstart;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.ResolveInfo;
+import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.text.method.SingleLineTransformationMethod;
 import android.util.Log;
 import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.View.OnClickListener;
@@ -41,9 +42,9 @@ import java.util.List;
 //Need the following import to get access to the app resources, since this
 //class is in a sub-package.
 
-public class AppGrid extends Activity implements OnItemClickListener {
+public class AppConfig extends Activity implements OnItemClickListener {
 
-	GridView mGrid;
+	GridView mList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,16 +52,14 @@ public class AppGrid extends Activity implements OnItemClickListener {
 
         loadApps(); // do this in onresume?
 
-        setContentView(R.layout.appgrid);
-        mGrid = (GridView) findViewById(R.id.myGrid);
-        mGrid.setAdapter(new AppsAdapter());
-        mGrid.setOnItemClickListener(this);   
+        setContentView(R.layout.applist);
+        mList = (GridView) findViewById(R.id.list);
+        mList.setAdapter(new AppsAdapter());
+        mList.setOnItemClickListener(this);   
         Button button = (Button)findViewById(R.id.Button01);
         button.setOnClickListener(Button01Listener);
         button = (Button)findViewById(R.id.Button02);
         button.setOnClickListener(Button02Listener);
-        button = (Button)findViewById(R.id.Button03);
-        button.setOnClickListener(Button03Listener);
     }
 
     private OnClickListener Button01Listener = new OnClickListener() {
@@ -69,8 +68,8 @@ public class AppGrid extends Activity implements OnItemClickListener {
 	        Bundle b = new Bundle();  
 	        b.putString("msg", "hide");  
 	        i.putExtras(b);  
-	        AppGrid.this.setResult(RESULT_OK, i);  
-	        AppGrid.this.finish();
+	        AppConfig.this.setResult(RESULT_OK, i);  
+	        AppConfig.this.finish();
         }
     };
 
@@ -80,20 +79,9 @@ public class AppGrid extends Activity implements OnItemClickListener {
 	        Bundle b = new Bundle();  
 	        b.putString("msg", "config");  
 	        i.putExtras(b);  
-	        AppGrid.this.setResult(RESULT_OK, i);  
-	        AppGrid.this.finish();
+	        AppConfig.this.setResult(RESULT_OK, i);  
+	        AppConfig.this.finish();
         }
-    };
-
-    private OnClickListener Button03Listener = new OnClickListener() {
-        public void onClick(View v) {
-			Intent i = getIntent();  
-	        Bundle b = new Bundle();  
-	        b.putString("msg", "quit");  
-	        i.putExtras(b);  
-	        AppGrid.this.setResult(RESULT_OK, i);  
-	        AppGrid.this.finish();
-		}
     };
 
     private List<ResolveInfo> mApps;
@@ -108,41 +96,45 @@ public class AppGrid extends Activity implements OnItemClickListener {
     //重点在这里面
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
         // TODO Auto-generated method stub
-        Log.e("grid1", "click");
-        ResolveInfo info =  mApps.get(position);
-        Log.e("grid1", info.activityInfo.packageName);
-
-        Intent intent = new Intent();
-        intent.setClassName(info.activityInfo.packageName, info.activityInfo.name);
-        startActivity(intent);
+        //重置上次颜色为Color.BLACK
+        LinearLayout lLayout = (LinearLayout)view;
+        lLayout.setBackgroundColor(Color.RED);
+        
+//        ImageView lImage = (ImageView)lLayout.getChildAt(0);
+//        TextView lText = (TextView)lLayout.getChildAt(1);
+        
+//        lImage.setBackgroundResource(R.drawable.lightning_128x128);
+//        lText.setTextColor(Color.RED);
+        
+        //保存最新的上次ID
     }
     
+
     public class AppsAdapter extends BaseAdapter {
         public AppsAdapter() {
         }
 
         public View getView(int position, View convertView, ViewGroup parent) {
-//            ImageView i;
-//            if (convertView == null) {
-//                i = new ImageView(AppGrid.this);
-//                i.setScaleType(ImageView.ScaleType.FIT_CENTER);
-//                i.setLayoutParams(new GridView.LayoutParams(50, 50));
-//            } else {
-//                i = (ImageView) convertView;
-//            }
-//
+
+        	// 从程序生成list里面的内容
+//            LinearLayout layout = new LinearLayout(AppConfig.this);  
+//            layout.setOrientation(LinearLayout.HORIZONTAL);  
+//              
 //            ResolveInfo info = mApps.get(position);
-//            i.setImageDrawable(info.activityInfo.loadIcon(getPackageManager()));
-//
-//            return i;
-            
-            LinearLayout layout = new LinearLayout(AppGrid.this);  
-            layout.setOrientation(LinearLayout.VERTICAL);  
+//            layout.addView(addTitleView(info.activityInfo.loadIcon(getPackageManager()),info.activityInfo.loadLabel(getPackageManager()).toString()));  
+//              
+//            return layout;
+
+        	// 从layout文件生成list里面的内容
+        	ResolveInfo info = mApps.get(position);
+            convertView = LayoutInflater.from(getApplicationContext()).inflate  
+            (R.layout.listitem,null);  
               
-            ResolveInfo info = mApps.get(position);
-            layout.addView(addTitleView(info.activityInfo.loadIcon(getPackageManager()),info.activityInfo.loadLabel(getPackageManager()).toString()));  
-              
-            return layout;              
+            TextView mTextView = (TextView)convertView.findViewById(R.id.imageTitle);  
+            mTextView.setText(info.activityInfo.loadLabel(getPackageManager()).toString());  
+            ImageView mImageView = (ImageView)convertView.findViewById(R.id.imageView);  
+            mImageView.setImageDrawable(info.activityInfo.loadIcon(getPackageManager()));  
+            return convertView;          	
         }
 
 
@@ -159,20 +151,20 @@ public class AppGrid extends Activity implements OnItemClickListener {
         }
 
         public View addTitleView(Drawable image, String title){  
-            LinearLayout layout = new LinearLayout(AppGrid.this);  
-            layout.setOrientation(LinearLayout.VERTICAL);  
+            LinearLayout layout = new LinearLayout(AppConfig.this);  
+            layout.setOrientation(LinearLayout.HORIZONTAL);  
               
-            ImageView iv = new ImageView(AppGrid.this);  
+            ImageView iv = new ImageView(AppConfig.this);  
             iv.setScaleType(ImageView.ScaleType.FIT_CENTER);
-          	iv.setLayoutParams(new GridView.LayoutParams(40, 40));
+          	iv.setLayoutParams(new GridView.LayoutParams(50, 50));
             iv.setImageDrawable(image);  
               
             layout.addView(iv);  
               
               
-            TextView tv = new TextView(AppGrid.this);
+            TextView tv = new TextView(AppConfig.this);
 //            tv.setTransformationMethod(SingleLineTransformationMethod.getInstance());  
-            tv.setSingleLine(true);
+//            tv.setSingleLine(true);
             tv.setText(title);  
               
             layout.addView(tv,  
--- src/com/redhorse/quickstart/quickstart.java
+++ src/com/redhorse/quickstart/quickstart.java
@@ -1,19 +1,89 @@
 package com.redhorse.quickstart;
 
 import com.redhorse.quickstart.AppGrid;
+import com.redhorse.quickstart.R;
 
 import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
 public class quickstart extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.main);
-		Intent setting = new Intent();
-		setting.setClass(this, AppGrid.class);
-		startActivity(setting);
-    }
+
+	private static final int STARTGRID_REQUEST = 0;
+	private static final int STARTCONFIG_REQUEST = 1;
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		// setContentView(R.layout.main);
+		Intent it = new Intent();
+		it.setClass(this, AppGrid.class);
+		// startActivity(it);
+		startActivityForResult(it, STARTGRID_REQUEST);
+		notification(this, "小红马快速启动：随时启动你的最爱!");
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+		Intent it = new Intent();
+		switch (requestCode) {
+		case STARTGRID_REQUEST:
+			switch (resultCode) {
+			case RESULT_OK:
+				Bundle b = data.getExtras();
+				String msg = b.getString("msg");
+				if (msg.equalsIgnoreCase("quit")) {
+					finish();
+				} else if (msg.equalsIgnoreCase("hide")) {
+
+				} else if (msg.equalsIgnoreCase("config")) {
+					it = new Intent();
+					it.setClass(this, AppConfig.class);
+					startActivityForResult(it, STARTCONFIG_REQUEST);
+				}
+				break;
+			default:
+				finish();
+				break;
+			}
+			break;
+		case STARTCONFIG_REQUEST:
+			it = new Intent();
+			it.setClass(this, AppGrid.class);
+			startActivityForResult(it, STARTGRID_REQUEST);
+			break;
+		default:
+			finish();
+			break;
+		}
+		Log.e("quickstart", "back");
+	}
+
+	private NotificationManager mNM;
+
+	private void notification(Context ctx, String msginfo) {
+		try {
+			mNM = (NotificationManager) ctx
+					.getSystemService(Context.NOTIFICATION_SERVICE);
+			Intent intent = new Intent(ctx, quickstart.class);
+			CharSequence appName = ctx.getString(R.string.app_name);
+			Notification notification = new Notification(R.drawable.icon_noborder,
+					appName, System.currentTimeMillis());
+			notification.flags = Notification.FLAG_NO_CLEAR;
+			CharSequence appDescription = msginfo;
+			notification.setLatestEventInfo(ctx, appName, appDescription,
+					PendingIntent.getActivity(ctx, 0, intent,
+							PendingIntent.FLAG_CANCEL_CURRENT));
+			mNM.notify(0, notification);
+		} catch (Exception e) {
+			mNM = null;
+		}
+	}
 }
